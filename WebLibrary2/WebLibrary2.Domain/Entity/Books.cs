99091dd74179308b776be9e414998aec5bd3c1e7
Mikhail Zelenskiy <zelenskiymo@gmail.com>
2017-09-17 17:17:55 +0300

    update 17/09/2017
    I've got a strange error. When I try to connect to my DB the program throw me an error, which text is "Cannot CREATE DATABASE because there is created database with the same name."

--- WebLibrary2/WebLibrary2.Domain/Entity/Books.cs
+++ WebLibrary2/WebLibrary2.Domain/Entity/Books.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace WebLibrary2.Domain.Entity
 {
     public class Book
-    {
+    {   [Key]
         public int idBook { get; set; }
         public int idAuthor { get; set; }
         public int idGenre { get; set; }
